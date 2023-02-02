4a59a38908ef781015a48a55a4a738931ad8ae8a
koobe <koobe.liu@uaf.com.cn>
2023-02-02 11:01:49 +0800

    生成基础代码-tpl

---
+++ templates/java/vo/req.java
@@ -0,0 +1,16 @@
+package {{package_name}}.bean;
+
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel("{{table_comment}}请求vo")
+public class {{class_name}}Req implements Serializable {
+{% for column in java_columns %}
+    @ApiModelProperty("{{column.column_comment}}")
+    private {{column.java_type}} {{column.java_name}}
+{% endfor %}
+}
